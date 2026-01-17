f6fd5b6b9ba1b57399baa8fd7095fabfe651172e
Riddhika rathore <riddhikarathore0101@gmail.com>
2026-01-17 23:12:03 +0530

    longest common prefix

--- script.js
+++ script.js
@@ -22,16 +22,38 @@
 //output =[11]-- target
 //5+6=11
 
-var twosum = function(nums,target){
-    for(let i = 0;i<nums.length;i++){
-        for(let j = i +1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){
-                return [nums[i],nums[j]]
-            }
+// var twosum = function(nums,target){
+//     for(let i = 0;i<nums.length;i++){
+//         for(let j = i +1;j<nums.length;j++){
+//             if(nums[i]+nums[j]==target){
+//                 return [nums[i],nums[j]]
+//             }
+//         }
+//     }
+// }
+//  var a = twosum([2,3,4,5,6],11)
+//  console.log(a)
+
+//longest common prefix 
+//problem number 14 in leetcode
+
+//input =["riddhika","riddhi","riddhu"]
+//output = ["riddh"]
+
+var longestCommonPrefix = function(strs) {
+    if(strs.length === 0) return ""
+    let prefix = strs[0];
+    for(let i = 1; i<strs.length;i++){
+        while(strs[i].indexOf(prefix)!==0){
+            prefix=prefix.slice(0,-1);
+            if(prefix == "") return "";
         }
     }
-}
- var a = twosum([2,3,4,5,6],11)
- console.log(a)
+    return prefix;
+};
 
- 
+var a = longestCommonPrefix(["riddhika","riddhi","riddhu"])
+
+    console.log(a);
+
+
