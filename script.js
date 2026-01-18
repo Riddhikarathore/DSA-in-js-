a68eda9f1961c70bada7a67311514e58b19f0822
Riddhika rathore <riddhikarathore0101@gmail.com>
2026-01-18 23:25:07 +0530

    remove element

--- script.js
+++ script.js
@@ -73,3 +73,18 @@ var removeDuplicates = function(nums) {
 
     return i + 1;  
 };
+
+//remove element question 27 
+
+var removeElement = function(nums, val) {
+    let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
