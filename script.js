1cc6e5eb4145536c29e7a9e29093511a71fb0faf
Riddhika rathore <riddhikarathore0101@gmail.com>
2026-01-19 23:00:47 +0530

    marge sorted array

--- script.js
+++ script.js
@@ -135,4 +135,31 @@ var plusOne = function(digits) {
 };
 
 
+//marge sorted array
+
+var merge = function(nums1, m, nums2, n) {
+
+     let i = m - 1;      // last valid index of nums1
+    let j = n - 1;      // last index of nums2
+    let k = m + n - 1;  // last index of nums1
+
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    // if nums2 still has elements
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+
+
 
